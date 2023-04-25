bc74d918e54af63193570d6319010ff18abb6e42
El-Behery <dev.elbehery@gmail.com>
2023-04-25 18:02:58 +0200

    preparing the products and fontawesome

---
+++ script/index.js
@@ -0,0 +1,5 @@
+import { popularProducts } from "../constants/popularProducts.js";
+import { onSaleProducts } from "../constants/onSaleProducts.js";
+
+console.log(popularProducts);
+console.log(onSaleProducts);
