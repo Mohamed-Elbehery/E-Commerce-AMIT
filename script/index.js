f1e5ad66b6d587709818f73332ca7eeaf388bb5c
El-Behery <dev.elbehery@gmail.com>
2023-04-26 12:43:02 +0200

    hero html and css, popular html and css

--- script/index.js
+++ script/index.js
@@ -1,5 +1,33 @@
+//! Imports
 import { popularProducts } from "../constants/popularProducts.js";
-import { onSaleProducts } from "../constants/onSaleProducts.js";
 
-console.log(popularProducts);
-console.log(onSaleProducts);
+//! Variables
+const popularProductsContainer = document.querySelector(".products");
+let popularProductsList = [];
+
+//! Functions
+const windowLoad = () => {
+  popularProductsList = popularProducts.map(
+    (product) =>
+      `
+      <div class="product">
+        <div>
+          <img productId="${product.id}" src="${product.personImg}" alt="${product.title}" />
+          <img productId="${product.id}" src="${product.img}" alt="${product.title}" />
+        </div>
+        <div>
+          <span>${product.category}</span>
+          <h3>${product.title}</h3>
+          <span>${product.price}</span>
+        </div>
+      </div>
+      `
+  );
+
+  popularProductsList.forEach((product) => {
+    popularProductsContainer.innerHTML += product;
+  });
+};
+
+//! Event Listeners
+window.addEventListener("load", windowLoad);
